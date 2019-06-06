1a95582c10ab3f6668b57b878f01cfff54ffd7ed
galiosmax <31752367+galiosmax@users.noreply.github.com>
2019-06-06 14:50:15 +0700

    supplyDao

--- PhotocentreDatabase2/src/main/kotlin/photocentre/dao/SupplyDao.kt
+++ PhotocentreDatabase2/src/main/kotlin/photocentre/dao/SupplyDao.kt
@@ -1,19 +1,27 @@
 package photocentre.dao
 
-import photocentre.dataClasses.Supplier
 import photocentre.dataClasses.Supply
 import java.sql.Statement
+import java.sql.Types.BIGINT
 import javax.sql.DataSource
 
 class SupplyDao(private val dataSource: DataSource) {
 
-    fun createSupply(toCreate: Supply): Long {
+    fun createSupply(supply: Supply): Long {
         val statement = dataSource.connection.prepareStatement(
-            "INSERT INTO supplies (supply_cost, supplier_id) VALUES (?, ?)",
-            Statement.RETURN_GENERATED_KEYS
+                "insert into supplies (supply_cost, supply_date, supply_completion_date, supplier_id) values (?, ?, ?, ?)",
+                Statement.RETURN_GENERATED_KEYS
         )
-        statement.setFloat(1, toCreate.cost)
-        statement.setLong(2, toCreate.supplier.id!!)
+
+        statement.setFloat(1, supply.cost)
+        statement.setDate(2, supply.date)
+        statement.setDate(3, supply.completionDate)
+        if (supply.supplier != null) {
+            statement.setLong(4, supply.supplier.id!!)
+        } else {
+            statement.setNull(4, BIGINT)
+        }
+
         statement.executeUpdate()
         val generated = statement.generatedKeys
         generated.next()
@@ -22,13 +30,19 @@ class SupplyDao(private val dataSource: DataSource) {
 
     fun createSupplies(toCreate: Iterable<Supply>): List<Long> {
         val statement = dataSource.connection.prepareStatement(
-            "INSERT INTO supplies (supply_cost, supplier_id) VALUES (?, ?)",
-            Statement.RETURN_GENERATED_KEYS
+                "insert into supplies (supply_cost, supply_date, supply_completion_date, supplier_id) values (?, ?, ?, ?)",
+                Statement.RETURN_GENERATED_KEYS
         )
 
         for (supply in toCreate) {
             statement.setFloat(1, supply.cost)
-            statement.setLong(2, supply.supplier.id!!)
+            statement.setDate(2, supply.date)
+            statement.setDate(3, supply.completionDate)
+            if (supply.supplier != null) {
+                statement.setLong(4, supply.supplier.id!!)
+            } else {
+                statement.setNull(4, BIGINT)
+            }
             statement.addBatch()
         }
 
@@ -42,34 +56,52 @@ class SupplyDao(private val dataSource: DataSource) {
         return res
     }
 
-    //TODO LAZY LOADING
-    /*fun findSupply(id: Long): Supply? {
-        val statement =
-            dataSource.connection.prepareStatement("SELECT supply_id, supply_cost, supplier_id FROM supplies WHERE supply_id = ?")
+    fun findSupply(id: Long): Supply? {
+        val statement = dataSource.connection.prepareStatement(
+                "select supply_id, supply_cost, supply_date, supply_completion_date, supplier_id from supplies where supply_id = ?"
+        )
+
         statement.setLong(1, id)
         val resultSet = statement.executeQuery()
+
+        val supplierDao = SupplierDao(dataSource)
+
         return if (resultSet.next()) {
             Supply(
-                    resultSet.getLong("supply_id"),
-                    resultSet.getFloat("supply_cost"),
-                    Supplier(resultSet.getLong("supplier_id"), )
+                    id = resultSet.getLong("supply_id"),
+                    cost = resultSet.getFloat("supply_cost"),
+                    date = resultSet.getDate("supply_date"),
+                    completionDate = resultSet.getDate("supply_completion_date"),
+                    supplier = supplierDao.findSupplier(resultSet.getLong("supplier_id"))
             )
         } else {
             null
         }
-    }*/
+    }
 
     fun updateSupply(supply: Supply) {
-        val statement =
-            dataSource.connection.prepareStatement("UPDATE supplies SET supply_cost = ?, supplier_id = ? WHERE supply_id = ?")
+        val statement = dataSource.connection.prepareStatement(
+                "update supplies set supply_cost = ?, supply_date = ?, supply_completion_date = ?, supplier_id = ? where supply_id = ?"
+        )
+
         statement.setFloat(1, supply.cost)
-        statement.setLong(2, supply.supplier.id!!)
-        statement.setLong(3, supply.id!!)
+        statement.setDate(2, supply.date)
+        statement.setDate(3, supply.completionDate)
+        if (supply.supplier != null) {
+            statement.setLong(4, supply.supplier.id!!)
+        } else {
+            statement.setNull(4, BIGINT)
+        }
+        statement.setLong(5, supply.id!!)
+
         statement.executeUpdate()
     }
 
     fun deleteSupply(id: Long) {
-        val statement = dataSource.connection.prepareStatement("DELETE FROM supplies WHERE supply_id = ?")
+        val statement = dataSource.connection.prepareStatement(
+                "delete from supplies where supply_id = ?"
+        )
+
         statement.setLong(1, id)
         statement.executeUpdate()
     }
