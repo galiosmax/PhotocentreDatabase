3f81c1f17582cf25299c126b0ccce26cc8ff5d54
galiosmax <31752367+galiosmax@users.noreply.github.com>
2019-06-06 13:48:40 +0700

    professionalDao

--- PhotocentreDatabase2/src/main/kotlin/photocentre/dao/ProfessionalDao.kt
+++ PhotocentreDatabase2/src/main/kotlin/photocentre/dao/ProfessionalDao.kt
@@ -3,17 +3,22 @@ package photocentre.dao
 import photocentre.dataClasses.BranchOffice
 import photocentre.dataClasses.Professional
 import java.sql.Statement
+import java.sql.Types.BIGINT
 import javax.sql.DataSource
 
 class ProfessionalDao(private val dataSource: DataSource) {
 
-    /*fun createProfessional(toCreate: Professional): Long {
+    fun createProfessional(professional: Professional): Long {
         val statement = dataSource.connection.prepareStatement(
-            "INSERT INTO professionals (professional_discount, branch_office_id) VALUES(?, ?)",
-            Statement.RETURN_GENERATED_KEYS
+                "insert into professionals (professional_discount, branch_office_id) values (?, ?)",
+                Statement.RETURN_GENERATED_KEYS
         )
-        statement.setInt(1, toCreate.discount)
-        statement.setLong(2, toCreate.branchOffice.id!!)
+        statement.setInt(1, professional.discount)
+        if (professional.branchOffice != null) {
+            statement.setLong(2, professional.branchOffice.id!!)
+        } else {
+            statement.setNull(2, BIGINT)
+        }
         statement.executeUpdate()
         val generated = statement.generatedKeys
         generated.next()
@@ -22,13 +27,17 @@ class ProfessionalDao(private val dataSource: DataSource) {
 
     fun createProfessionals(toCreate: Iterable<Professional>): List<Long> {
         val statement = dataSource.connection.prepareStatement(
-            "INSERT INTO professionals (professional_discount) VALUES(?)",
-            Statement.RETURN_GENERATED_KEYS
+                "insert into professionals (professional_discount, branch_office_id) values (?, ?)",
+                Statement.RETURN_GENERATED_KEYS
         )
 
         for (professional in toCreate) {
             statement.setInt(1, professional.discount)
-            statement.setLong(2, professional.branchOffice.id!!)
+            if (professional.branchOffice != null) {
+                statement.setLong(2, professional.branchOffice.id!!)
+            } else {
+                statement.setNull(2, BIGINT)
+            }
             statement.addBatch()
         }
 
@@ -42,17 +51,21 @@ class ProfessionalDao(private val dataSource: DataSource) {
         return res
     }
 
-    //TODO LAZY LOADING
     fun findProfessional(id: Long): Professional? {
-        val statement =
-            dataSource.connection.prepareStatement("SELECT professional_id, professional_discount, branch_office_id FROM professionals WHERE professional_id = ?")
+        val statement = dataSource.connection.prepareStatement(
+                "select professional_id, professional_discount, branch_office_id from professionals where professional_id = ?"
+        )
+
         statement.setLong(1, id)
         val resultSet = statement.executeQuery()
+
+        val branchOfficeDao = BranchOfficeDao(dataSource)
+
         return if (resultSet.next()) {
             Professional(
-                    resultSet.getLong("professional_id"),
-                    resultSet.getInt("professional_discount"),
-                    BranchOffice(resultSet.getLong("branch_office_id"), )
+                    id = resultSet.getLong("professional_id"),
+                    discount = resultSet.getInt("professional_discount"),
+                    branchOffice = branchOfficeDao.findBranchOffice(resultSet.getLong("branch_office_id"))
             )
         } else {
             null
@@ -60,17 +73,29 @@ class ProfessionalDao(private val dataSource: DataSource) {
     }
 
     fun updateProfessional(professional: Professional) {
-        val statement =
-            dataSource.connection.prepareStatement("UPDATE professionals SET professional_discount = ?, branch_office_id = ? WHERE professional_id = ?")
+        val statement = dataSource.connection.prepareStatement(
+                "update professionals set professional_discount = ?, branch_office_id = ? where professional_id = ?"
+        )
+
         statement.setInt(1, professional.discount)
-        statement.setLong(2, professional.branchOffice.id!!)
+        if (professional.branchOffice != null) {
+            statement.setLong(2, professional.branchOffice.id!!)
+        } else {
+            statement.setNull(2, BIGINT)
+        }
         statement.setLong(3, professional.id!!)
+
         statement.executeUpdate()
     }
 
     fun deleteProfessional(id: Long) {
-        val statement = dataSource.connection.prepareStatement("DELETE FROM professionals WHERE professional_id = ?")
+        val statement = dataSource.connection.prepareStatement(
+                "delete from professionals where professional_id = ?"
+        )
+
         statement.setLong(1, id)
         statement.executeUpdate()
-    }*/
+    }
+
+
 }
